8a931dd58b1c1a8ec1f5d7389b3d862592d4773d
sohamantrolikar <sohamantrolikar@gmail.com>
2019-10-19 02:39:49 +0530

    Hand detection with chord

--- Hand_detection.py
+++ Hand_detection.py
@@ -3,11 +3,39 @@ import numpy as np
 import pyautogui as mouse
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1980)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 hand_cascade = cv2.CascadeClassifier('hand.xml')
 mouse.FAILSAFE = False
 count = 0
+prevx=0
+prevy=0
+import math
+
+def getPoints(x1,y1,x2,y2):
+    points=[]
+    if(x1==x2 and y1==y2):
+        return points
+    dist=(y2-y1)*(y2-y1) + (x2-x1)*(x2-x1)
+    dist=math.sqrt(dist)
+
+    step=dist/1
+    if(x1==x2):
+        while(y1<y2):
+            points.append((x1,y1))
+            y1+=step
+        return points
+
+    slope=(y2-y1)/(x2-x1)
+    theta=math.atan(slope)
+    cos=math.cos(theta)
+    sin=math.sin(theta)
+    while dist>0:
+        x1+=step*cos
+        y1+=step*sin
+        points.append((x1,y1))
+        dist-=step
+    return points
 
 while True:
     ret, frame = cap.read()
@@ -30,11 +58,15 @@ while True:
             cv2.putText(img=frame, text='Detected', org=(int(100 / 2 - 20), int(100 / 2)),
                         fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1,
                         color=(0, 255, 0))
-            for (x, y, w, h) in hands:
-                print(x, y)
-                mouse.moveTo(x, y)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+            for (x, y, w, h) in hands:
+                l = getPoints(prevx,prevy,x,y)
+                for i in l:
+                    mouse.moveTo(i[0], i[1] )
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                print(l)
+                prevx=x
+                prevy=y
         elif len(contour) == 0:
             cv2.putText(img=frame, text='Where are you?', org=(int(100 / 2 - 20), int(100 / 2)),
                         fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1,
